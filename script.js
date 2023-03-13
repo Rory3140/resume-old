c3c0362509d0ef000d348bc574b685cf8094f507
Rory Wood <rorywood9@live.com>
2023-03-12 23:03:29 -0400

    fixed scrolldown javascript

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-console.log("script.js Linked");
+console.log('script.js Linked');
+
+var skillsElement = document.getElementById('home-link');
+skillsElement.onclick = function () { scrollAbove('title') };
+
+var skillsElement = document.getElementById('skills-link');
+skillsElement.onclick = function () { scrollAbove('skills') };
+
+var skillsElement = document.getElementById('education-link');
+skillsElement.onclick = function () { scrollAbove('education') };
+
+var skillsElement = document.getElementById('work-experience-link');
+skillsElement.onclick = function () { scrollAbove('work-experience') };
+
+var skillsElement = document.getElementById('about-link');
+skillsElement.onclick = function () { scrollAbove('about') };
+
+var skillsElement = document.getElementsByClassName('contact-link');
+for (let i = 0; i < skillsElement.length; i++) {
+  skillsElement[i].onclick = function () { scrollAbove('contact') };
+}
 
 // Scroll on link click
-function scrollAbove(id, margin = 49) {
-  var element = document.querySelector(id);
+function scrollAbove(id) {
+  let margin = 49;
+  var element = document.getElementById(id);
   let dims = element.getBoundingClientRect();
   window.scrollTo(window.scrollX, dims.top + window.scrollY - margin);
 }
 
 var navbar = document.querySelector('#navbar');
-//var titlePage = document.querySelector('#title');
 var navDim = navbar.offsetTop;
-//var titleDim = titlePage.offsetHeight - navbar.offsetHeight;
 
 window.onscroll = function () {
   // Sticky navbar
@@ -19,10 +38,6 @@ window.onscroll = function () {
   } else {
     navbar.classList.remove("sticky");
   }
-
-  // if (window.pageYOffset >= titleDim) {
-  //   console.log("test");
-  // }
 };
 
 
