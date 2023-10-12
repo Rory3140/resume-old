fcfc238657c8c604aa9eaf038d506bee9c3f440d
Rory Wood <rorywood9@live.com>
2023-10-12 11:56:19 -0400

    reformatted code

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-console.log('script.js Linked');
+console.log("script.js Linked");
 
-var navbar = document.querySelector('#navbar');
+var navbar = document.querySelector("#navbar");
 var navDim = navbar.offsetTop;
 
 window.onscroll = function () {
   // Sticky navbar
   if (window.pageYOffset >= navDim) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 };
 
 // Links scrollAbove function to link elements
-var homeElement = document.getElementById('home-link');
-homeElement.onclick = function () { scrollAbove('title') };
+var homeElement = document.getElementById("home-link");
+homeElement.onclick = function () {
+  scrollAbove("title");
+};
 
-var skillsElement = document.getElementById('skills-link');
-skillsElement.onclick = function () { scrollAbove('skills') };
+var skillsElement = document.getElementById("skills-link");
+skillsElement.onclick = function () {
+  scrollAbove("skills");
+};
 
-var educationElement = document.getElementById('education-link');
-educationElement.onclick = function () { scrollAbove('education') };
+var educationElement = document.getElementById("education-link");
+educationElement.onclick = function () {
+  scrollAbove("education");
+};
 
-var workElement = document.getElementById('work-experience-link');
-workElement.onclick = function () { scrollAbove('work-experience') };
+var workElement = document.getElementById("work-experience-link");
+workElement.onclick = function () {
+  scrollAbove("work-experience");
+};
 
-var aboutElement = document.getElementById('about-link');
-aboutElement.onclick = function () { scrollAbove('about') };
+var aboutElement = document.getElementById("about-link");
+aboutElement.onclick = function () {
+  scrollAbove("about");
+};
 
-var contactElements = document.getElementsByClassName('contact-link');
+var contactElements = document.getElementsByClassName("contact-link");
 for (let i = 0; i < contactElements.length; i++) {
-  contactElements[i].onclick = function () { scrollAbove('contact') };
+  contactElements[i].onclick = function () {
+    scrollAbove("contact");
+  };
 }
 
 // Scroll on link click
@@ -42,46 +54,66 @@ function scrollAbove(id) {
 }
 
 // Links icon and overlay to show and hide function
-var overlayElement = document.getElementById('overlay');
-overlayElement.onclick = function () { hideOverlay() };
+var overlayElement = document.getElementById("overlay");
+overlayElement.onclick = function () {
+  hideOverlay();
+};
 
-var javaElement = document.getElementById('java');
-javaElement.onclick = function () { showOverlay('java-content') };
+var javaElement = document.getElementById("java");
+javaElement.onclick = function () {
+  showOverlay("java-content");
+};
 
-var javascriptElement = document.getElementById('javascript');
-javascriptElement.onclick = function () { showOverlay('javascript-content') };
+var javascriptElement = document.getElementById("javascript");
+javascriptElement.onclick = function () {
+  showOverlay("javascript-content");
+};
 
-var cplusElement = document.getElementById('cplus');
-cplusElement.onclick = function () { showOverlay('cplus-content') };
+var cplusElement = document.getElementById("cplus");
+cplusElement.onclick = function () {
+  showOverlay("cplus-content");
+};
 
-var htmlElement = document.getElementById('html');
-htmlElement.onclick = function () { showOverlay('html-content') };
+var htmlElement = document.getElementById("html");
+htmlElement.onclick = function () {
+  showOverlay("html-content");
+};
 
-var cssElement = document.getElementById('css');
-cssElement.onclick = function () { showOverlay('css-content') };
+var cssElement = document.getElementById("css");
+cssElement.onclick = function () {
+  showOverlay("css-content");
+};
 
-var sqlElement = document.getElementById('sql');
-sqlElement.onclick = function () { showOverlay('sql-content') };
+var sqlElement = document.getElementById("sql");
+sqlElement.onclick = function () {
+  showOverlay("sql-content");
+};
 
-var pythonElement = document.getElementById('python');
-pythonElement.onclick = function () { showOverlay('python-content') };
+var pythonElement = document.getElementById("python");
+pythonElement.onclick = function () {
+  showOverlay("python-content");
+};
 
-var seleniumElement = document.getElementById('selenium');
-seleniumElement.onclick = function () { showOverlay('selenium-content') };
+var seleniumElement = document.getElementById("selenium");
+seleniumElement.onclick = function () {
+  showOverlay("selenium-content");
+};
 
-var nodeElement = document.getElementById('node');
-nodeElement.onclick = function () { showOverlay('node-content') };
+var nodeElement = document.getElementById("node");
+nodeElement.onclick = function () {
+  showOverlay("node-content");
+};
 
 function showOverlay(id) {
-  overlayElement.style.display = 'flex';
-  document.getElementById(id).style.display = 'block';
+  overlayElement.style.display = "flex";
+  document.getElementById(id).style.display = "block";
 }
 
 function hideOverlay() {
-  overlayElement.style.display = 'none';
+  overlayElement.style.display = "none";
 
-  var contentElements = overlayElement.getElementsByClassName('content');
+  var contentElements = overlayElement.getElementsByClassName("content");
   for (let i = 0; i < contentElements.length; i++) {
-    contentElements[i].style.display = 'none';
+    contentElements[i].style.display = "none";
   }
 }
